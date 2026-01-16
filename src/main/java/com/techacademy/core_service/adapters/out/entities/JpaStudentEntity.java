b62cd6656beef0e87a4ac3af91aab2168a636815
Ian Gurgel <iangurgel25@hotmail.com>
2026-01-16 19:25:18 -0300

    feat: JpaStudentEntity; build: pom.xml, add Lombok dependency

--- src/main/java/com/techacademy/core_service/adapters/out/entities/JpaStudentEntity.java
+++ src/main/java/com/techacademy/core_service/adapters/out/entities/JpaStudentEntity.java
@@ -1,4 +1,26 @@
 package com.techacademy.core_service.adapters.out.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_students")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class JpaStudentEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Long classroomId;
+    private String registration;
 }
