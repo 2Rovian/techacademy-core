704b1048b60be24727f376dba3199f7a3ee3a9a0
Ian Gurgel <iangurgel25@hotmail.com>
2026-01-16 19:40:32 -0300

    feat: core interface StudentRepository from domain/student

---
+++ src/main/java/com/techacademy/core_service/domain/student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.techacademy.core_service.domain.student;
+
+import java.util.List;
+
+public interface StudentRepository {
+    Student registerStudent(Student student);
+    Student findStudentById(Long id);
+    List<Student> findAllStudents();
+    void deleteStudentById(Long id);
+}
